eb984bf73eb652b4179e8feb8d5d57c56ae1203a
Lionel La Rocca <lionel.larocca@proton.me>
2025-12-08 11:52:33 +0100

    feat: Add plot_mean_spectrum_windows to module exports

--- src/pymsix/__init__.py
+++ src/pymsix/__init__.py
@@ -2,6 +2,7 @@
 from .utils import validate, errors
 from .core.msicube import MSICube
 from .plotting.ion_images import plot_ion_images
+from .plotting.spectrum import plot_mean_spectrum_windows
 
 __all__ = [
     "MSICube",
